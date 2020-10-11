59f29816a5abb657d9e46fcdb9e5b87d2277ac3d
alphasr <nilaybaranwal@gmail.com>
2020-10-12 00:19:33 +0200

    Initial commit

---
+++ socket.js
@@ -0,0 +1,22 @@
+const socket = io => {
+    io.on('connection', client => {
+      console.log('New Connection');
+  
+      // socket event for client subscription
+      client.on('subscribeToDateEvent', interval => {
+        console.log('Client is subscribing with interval: ', interval);
+        
+        // emit message to the client side
+        setInterval(() => {
+          const payload = {
+            date: new Date().toUTCString(),
+            msg:'Logging successfully.'
+          }
+          client.emit('getDate', payload);
+          
+        }, interval);
+      });
+    });
+  }
+  
+  module.exports = socket;
---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import io from "socket.io-client";
+function App() {
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
+ 
+  // establish socket connection
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [dt, setDt] = useState('');
+  const [msg, setMsg] = useState('');
+  // establish socket connection
+  useEffect(() => {
+    setSocket(io('http://localhost:4000'));
+  }, []);
+
+  // subscribe to the socket event
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on('connect', () => {
+      setSocketConnected(socket.connected);
+      subscribeToDateEvent();
+    });
+    socket.on('disconnect', () => {
+      setSocketConnected(socket.connected);
+    });
+
+    socket.on("getDate", (data:any) => {
+      setDt(data.date);
+      setMsg(data.msg);
+    });
+
+   
+
+
+  }, [socket]);
+
+  // manage socket connection
+  const handleSocketConnection = () => {
+    if (socketConnected)
+      socket!.disconnect();
+    else {
+      socket!.connect();
+    }
+  }
+
+  // subscribe to socket date event
+  const subscribeToDateEvent = (interval = 1000) => {
+    socket!.emit('subscribeToDateEvent', interval);
+  }
+
+  return (
+    <div>
+      <h2>Welcome to Down-Spiral a data visualizer/debugger </h2>
+
+      <div><b>Connection status:</b> {socketConnected ? 'Connected' : 'Disconnected'}</div>
+      <input
+        type="button"
+        style={{ marginTop: 10 }}
+        value={socketConnected ? 'Disconnect' : 'Connect'}
+        onClick={handleSocketConnection} />
+
+      <div style={{ marginTop: 20 }}><b>Date: </b> {dt}</div>
+  <div style={{ marginTop: 20 }}><b>Message: </b>{msg}</div>
+
+    </div>
+  );
+}
+
+export default App;
