f7b5844b417abab6209a355eac981fb7ea76ca85
alphasr <nilaybaranwal@gmail.com>
2020-12-29 23:09:59 +0100

    fixed html renderer

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,54 +1,54 @@
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8000');
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:8000");
 
 function spiralLogs(cb: Function) {
-  socket.on('connect', function () {
+  socket.on("connect", function () {
     try {
-      socket.emit('room', 'myRoom');
-      console.log('entered room');
+      socket.emit("room", "myRoom");
+      console.log("entered room");
     } catch (error) {
       return spiralLogs(cb);
     }
   });
-  socket.on('setTableHeader', (data: any) => cb(data));
-  socket.on('setTableData', (data: any) => cb(data));
+  socket.on("setTableHeader", (data: any) => cb(data));
+  socket.on("setTableData", (data: any) => cb(data));
 }
 
 function spiralGraphs(cb: Function) {
-  console.log('inside graph logs');
-  socket.on('connect', function () {
+  console.log("inside graph logs");
+  socket.on("connect", function () {
     try {
-      socket.emit('room', 'myRoom');
-      console.log('entered room');
+      socket.emit("room", "myRoom");
+      console.log("entered room");
     } catch (error) {
       return spiralLogs(cb);
     }
   });
-  socket.on('setGraph', (data: any) => cb(data));
+  socket.on("setGraph", (data: any) => cb(data));
 }
 function simplePrinter(cb: Function) {
-  console.log('inside simple logs');
-  socket.on('connect', function () {
+  console.log("inside simple logs");
+  socket.on("connect", function () {
     try {
-      socket.emit('room', 'myRoom');
-      console.log('entered room');
+      socket.emit("room", "myRoom");
+      console.log("entered room");
     } catch (error) {
       return simplePrinter(cb);
     }
   });
-  socket.on('setSimplePrinter', (data: any) => cb(data));
+  socket.on("setSimplePrinter", (data: any) => cb(data));
 }
 function htmlPrinter(cb: Function) {
-  console.log('inside html logs');
-  socket.on('connect', function () {
+  console.log("inside html logs");
+  socket.on("connect", function () {
     try {
-      socket.emit('room', 'myRoom');
-      console.log('entered room');
+      socket.emit("room", "myRoom");
+      console.log("entered room");
     } catch (error) {
-      return simplePrinter(cb);
+      return htmlPrinter(cb);
     }
   });
-  socket.on('setHtmlPrinter', (data: any) => cb(data));
+  socket.on("setHtmlPrinter", (data: any) => cb(data));
 }
 
 //   socket.on("test", (timestamp) => cb(null, timestamp));
--- frontend/src/pages/HtmlPrinter.tsx
+++ frontend/src/pages/HtmlPrinter.tsx
@@ -1,7 +1,44 @@
-import React, { Fragment } from 'react';
-import HtmlPrinterSessions from '../components/html/HtmlPrinterSession';
+import React, { Dispatch, Fragment, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { htmlPrinter } from "../api";
+import HtmlPrinterSessions from "../components/html/HtmlPrinterSession";
+import {
+  IHtmlPrinterLogActions,
+  setHtmlPrinterLog,
+} from "../store/actions/htmlPrinterActions";
+import { IHtmlPrinterPayload } from "../store/reducers/htmlReducer";
 
 const HtmlPrinter = () => {
+  //   const { header, data } = useSelector((state: AppState) => state.tableLog);
+  const htmlPrinterDispatch = useDispatch<Dispatch<IHtmlPrinterLogActions>>();
+
+  // const getData = (payload: any) => {
+  //   let data: any[] = [];
+  //   for (const key in payload) {
+  //     data.push(<td>{payload[key]}</td>);
+  //   }
+
+  //   return data.map((datum) => datum);
+  // };
+
+  useEffect(() => {
+    const handleSetPayload = (payload: string) => {
+      console.log("payload = ", payload);
+      const parsedPayload: any = JSON.parse(payload);
+      console.log("data after parsing", JSON.stringify(parsedPayload.data));
+      if (parsedPayload.sessionId) {
+        const payload: IHtmlPrinterPayload = {
+          sessionId: parsedPayload.sessionId,
+          htmlPayload: parsedPayload.html,
+        };
+
+        console.log("data = ", JSON.stringify(payload));
+        htmlPrinterDispatch(setHtmlPrinterLog(payload));
+      }
+      return null;
+    };
+    htmlPrinter((payload: string) => handleSetPayload(payload));
+  }, [htmlPrinterDispatch]);
   return (
     <Fragment>
       <HtmlPrinterSessions />
